fca5adf066ee363e4002502ae3d39192156044be
MiketaNyoroN <mikenyoron@gmail.com>
2020-02-02 17:31:29 +0900

    [DO NOT MERGE] implement color image viewer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Kinect;
 
 namespace KineticController
 {
@@ -20,9 +12,78 @@ namespace KineticController
     /// </summary>
     public partial class MainWindow : Window
     {
+        private KinectSensor activeSensor;
+        private WriteableBitmap outputImage;
+
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            KinectSensor.KinectSensors.StatusChanged += KinectStatusChanged;
+
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                ConnectSensor(KinectSensor.KinectSensors[0]);
+            }
+        }
+
+        private void KinectStatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            Debug.WriteLine("Kinect status changed: {0}=>{1}", e.Sensor.DeviceConnectionId, e.Sensor.Status);
+
+            switch (e.Sensor.Status)
+            {
+                case KinectStatus.Connected:
+                    ConnectSensor(e.Sensor);
+                    break;
+                case KinectStatus.Disconnected:
+                    DisconnectSensor(e.Sensor);
+                    break;
+            }
+        }
+
+        private void ConnectSensor(KinectSensor sensor)
+        {
+            if (activeSensor != null) { return; }
+
+            activeSensor = sensor;
+
+            outputImage = new WriteableBitmap(640, 480, 96, 96, PixelFormats.Bgr32, null);
+            ColorImage.Source = outputImage;
+            activeSensor.ColorFrameReady += OnColorFrameReady;
+            activeSensor.ColorStream.Enable();
+
+            activeSensor.Start();
+            
+        }
+
+        private void OnColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame frame = e.OpenColorImageFrame())
+            {
+                if (frame == null) { return; }
+                outputImage.WritePixels(
+                    new Int32Rect(0, 0, 640, 480),
+                    frame.GetRawPixelData(),
+                    640 * frame.BytesPerPixel,
+                    0);
+            }
+        }
+
+        private void DisconnectSensor(KinectSensor sensor)
+        {
+            if (activeSensor == null) { return; }
+            if (activeSensor.DeviceConnectionId != sensor.DeviceConnectionId) { return; }
+
+            sensor.Stop();
+        }
+
+        private void Window_Closed(object sender, System.EventArgs e)
+        {
+            DisconnectSensor(activeSensor);
         }
     }
 }
